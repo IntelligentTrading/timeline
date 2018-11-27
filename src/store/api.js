45034c8e89c2f228e2fe5811b893c0a980f6c766
Francesco De Lisi <francesco@intelligenttrading.org>
2018-11-27 15:53:21 +0700

    Change fetch with request-promise

--- src/store/api.js
+++ src/store/api.js
@@ -1,5 +1,6 @@
 const coreApiUrl = `https://${process.env.ITT_API_HOST}/v2`
 const coreApiKey = process.env.ITT_API_KEY
+const rp = require('request-promise')
 
 const horizons = ['1h', '4h', '24h']
 const sources = ['POLONIEX', 'BITTREX', 'BINANCE']
@@ -8,16 +9,17 @@ const counterCurrencies = ['BTC', 'ETH', 'USDT', 'XMR']
 export default {
     getHistory: async (ticker, horizon, source, counterCurrency = 0) => {
 
-        return fetch(`${coreApiUrl}/signals/?source=${sources.indexOf(source.toUpperCase())}&transaction_currency=${ticker}&counter_currency=${counterCurrencies.indexOf(counterCurrency.toUpperCase())}&horizon=${horizons.indexOf(horizon)}`, {
+        var options = {
+            uri: `${coreApiUrl}/signals/?source=${sources.indexOf(source.toUpperCase())}&transaction_currency=${ticker}&counter_currency=${counterCurrencies.indexOf(counterCurrency.toUpperCase())}&horizon=${horizons.indexOf(horizon)}`,
             headers: {
                 "API-KEY": coreApiKey,
                 "Content-Type": "application/json"
             },
-            mode: "cors"
-        }).then(result => {
-            return result.json().then(data => {
-                return data;
-            });
+            json: true
+        }
+
+        return rp(options).then(data => {
+            return data;
         }).catch(err => console.log(err));
     },
     getHistoryPrices: async (ticker, source, counterCurrency = 0) => {
