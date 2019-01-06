16f97a40623a9c78c915b3151111ed7f81045da1
Francesco De Lisi <francesco@intelligenttrading.org>
2019-01-06 16:03:28 +0700

    Fixed variables to import

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,4 +3,7 @@ module.exports = {
   NODE_ENV: '"production"',
   ITT_API_HOST: `"${process.env.ITT_API_HOST}"`,
   ITT_API_KEY: `"${process.env.ITT_API_KEY}"`,
+  ITT_NODE_SERVICES: `"${process.env.ITT_NODE_SERVICES}"`,
+  NODE_SVC_API_KEY: `"${process.env.NODE_SVC_API_KEY}"`,
+  TELEGRAM_BOT_NAME: `"${process.env.TELEGRAM_BOT_NAME.replace('@','')}"`
 }
