0e0f89a26d064c45eafbed4f6de11d7ae5292edc
Francesco De Lisi <francesco@intelligenttrading.org>
2019-01-15 20:28:10 +0700

    fixed auth

--- src/router/auth.js
+++ src/router/auth.js
@@ -7,6 +7,7 @@ export default {
     if (!itfUser) return false;
 
     itfUser = JSON.parse(itfUser);
-    return ItfUser.isPro(itfUser) && itfUser.auth_date && moment(itfUser.auth_date).add(12,'hours').isAfter(Date.now());
+    // ItfUser.isPro(itfUser) 
+    return itfUser.eula && itfUser.auth_date && moment(itfUser.auth_date).add(12,'hours').isAfter(Date.now());
   }
 };
